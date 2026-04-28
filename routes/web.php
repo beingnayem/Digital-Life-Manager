08eda509bb5585346a0103879237d96a14efcea5
beingnayem <nayemsarwar12345@gmail.com>
2026-04-28 13:51:08 +0600

    feat: define authenticated routes and resource group

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,27 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\MoodController;
+use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::resource('tasks', TaskController::class)->except(['show']);
+    Route::resource('expenses', ExpenseController::class)->except(['show']);
+    Route::resource('notes', NoteController::class)->except(['show']);
+    Route::resource('moods', MoodController::class)->except(['show']);
+
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
